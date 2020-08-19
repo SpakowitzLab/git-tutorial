8b4d14792d146c3528c245745faf1985df8e8aed
Bruno Beltran <brunobeltran0@gmail.com>
2020-08-19 09:58:32 -0700

    API: subtract method parameter order flipped

--- mymath/arith.py
+++ mymath/arith.py
@@ -2,7 +2,7 @@ def add(x, y):
     return x + y
 
 def subtract(x, y):
-    return x - y
+    return y - x
 
 def double(x):
     return add(x, x)
--- test_arith.py
+++ test_arith.py
@@ -3,7 +3,7 @@ def test_add():
     assert add(1, 1) == 2
 
 def test_subtract():
-    assert subtract(2, 1) == 1
+    assert subtract(2, 1) == -1
 
 def test_double():
     assert double(3) == 6
