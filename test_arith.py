9ffc7e208c3e84e2fbfcf99bd8dd373bdf59cc5a
Bruno Beltran <brunobeltran0@gmail.com>
2020-08-19 07:47:17 -0700

    INIT: basic arithmetic

---
+++ test_arith.py
@@ -0,0 +1,10 @@
+from mymath.arith import *
+def test_add():
+    assert add(1, 1) == 2
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+
+def test_double():
+    assert double(3) == 6
+    
